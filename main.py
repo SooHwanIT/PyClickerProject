be92b2f716797a34887655b809a45d911eb843f7
ghks3691 <ghks3691@naver.com>
2021-06-13 00:52:33 +0900

    Upgrade 벨런스 구현

--- main.py
+++ main.py
@@ -38,23 +38,20 @@ def Button_OnClick():  # 버튼을 누를때마다 실행되는 함수
     gold += gold_per_click  # 골드를 1 늘려줌 (GPC로 변경 예정 )
     gold_text.set(gold)  # 골드를 표시해주는 골드 텍스트를 업데이트 해줌
 
-
 gold_Button = Button(window, text='Button', command=Button_OnClick).pack(
     side=BOTTOM, anchor=CENTER)  # 골드 버튼을 만들고 누를때마다 함수를 실행시킴
 
 
 class Auto_Upgade_button():
     def __init__(self, name, cost, cost_pow, upgade_pow):  # 이름, 비용, 비용 증가량, 업그레이드 수치
-        self.Sname = name
-        
+        self.name = name
         self.level = 1
-        self.name = self.level
         self.cost = cost
         self.cost_pow = cost_pow
         self.upgade_pow = upgade_pow
-        self.name = StringVar()
-        self.name.set("loding")
-        self.button = Button(window, textvariable=self.name, command=self.Buy_Auto_Gold).pack(
+        self.text = StringVar()
+        self.text.set("loding")
+        self.button = Button(window, textvariable=self.text, command=self.Buy_Auto_Gold).pack(
             side=TOP, anchor=S)
         self.UI_Update()
 
@@ -69,29 +66,38 @@ class Auto_Upgade_button():
             self.UI_Update()
         
     def UI_Update(self):
-        self.name.set(str(self.Sname) +".Lv"+ str(self.level) + "\n 비용 : " + str(int(self.cost)) + "\ngold/sec 증가량: " + str(self.upgade_pow))
+        self.text.set(str(self.name) +".Lv"+ str(self.level) + "\n 비용 : " + str(int(self.cost)) + "\ngold/sec 증가량: " + str(self.upgade_pow))
         gold_per_sec_text.set(gold_per_sec)
         gold_text.set(gold)
 
 
 class Click_Upgade_button():
     def __init__(self, name, cost, cost_pow, upgade_pow):  # 이름, 비용, 비용 증가량, 업그레이드 수치
-        self.cost = cost
-        self.power = cost_pow
-        self.upgade_pow = upgade_pow
-        self.name = name + "\n 비용 : " + \
-            str(cost) + "gold/click 증가량: " + str(upgade_pow)
-        self.button = Button(
-            window, text=self.name, command=self.Buy_Click_Upgade).pack(side=TOP, anchor=S)
-
+        self.name = name #이름
+        self.level = 1 #레벨
+        self.cost = cost #업그레이드비용 
+        self.cost_pow = cost_pow #업그레이드 비용 증가량
+        self.upgade_pow = upgade_pow #업그레이드 수치
+        self.text =  StringVar() # 버튼 text 
+        self.text.set('loding') #text 초기화
+        self.button = Button(  #버튼 생성
+            window, textvariable=self.text, command=self.Buy_Click_Upgade).pack(side=TOP, anchor=S)
+        self.UI_Update() #UI 업데이트
+            
     def Buy_Click_Upgade(self):
         global gold  # 골드를 글로벌 함수로 지정해서 참조할수 있게 만듬
         global gold_per_click
         if gold >= self.cost:  # 돈이 충분한가 확인
             gold -= self.cost  # 돈 차감
+            self.level += 1  # 레벨 1 증가
+            self.cost += int(self.level*self.cost_pow)  # 비용 증가 (레벨*코스트파워 만큼 추가)
             gold_per_click += self.upgade_pow  # 업그레이드 수치만큼 골드 퍼 클릭 증가
-            gold_per_click_text.set(gold_per_click)  # 라벨 업데이트
-            gold_text.set(gold)  # 라벨 업데이트
+            self.UI_Update()
+            
+    def UI_Update(self):
+        self.text.set(str(self.name) +".Lv"+ str(self.level) + "\n 비용 : " + str(int(self.cost)) + "\ngold/click증가량: " + str(self.upgade_pow))
+        gold_per_click_text.set(gold_per_click)  # 라벨 업데이트
+        gold_text.set(gold)  # 라벨 업데이트
 
 
 Cbt1 = Click_Upgade_button("test1", 10, 1.2, 1)
@@ -103,14 +109,14 @@ Abt2 = Auto_Upgade_button("test2", 20, 1.2, 2)
 Abt3 = Auto_Upgade_button("test3", 30, 1.2, 3)
 
 
-def Auto_Gold():  # 1초마다
-    global gold
-    global gold_per_sec
-    gold += gold_per_sec
-    gold_text.set(gold)
+def Auto_Gold():  # 1초마다 실행되는 함수
+    global gold #글로벌로 골드 선언 
+    global gold_per_sec #글로벌로 GPS 선언 
+    gold += gold_per_sec #골드를 GPS만큼 증가
+    gold_text.set(gold) #라벨 업데이트
     threading.Timer(1, Auto_Gold).start()  # 1초마다 재귀실행
+    #함수 마지막에 1초 후에 다시 동일 함수를 실행함으로써 1초마다 재귀실행되는 함수 생성
 
-
-Auto_Gold()
+Auto_Gold() #함수 실행
 
 window.mainloop()
